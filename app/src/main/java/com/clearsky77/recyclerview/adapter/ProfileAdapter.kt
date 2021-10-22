5cf257cd800f4318a2c7079169a1f812778db5b4
clearsky77 <clearsky3264@gmail.com>
2021-10-23 01:03:49 +0900

    Gradle에 recyclerview 추가 / 어댑터 작성 초기

---
+++ app/src/main/java/com/clearsky77/recyclerview/adapter/ProfileAdapter.kt
@@ -0,0 +1,6 @@
+package com.clearsky77.recyclerview.adapter
+
+import androidx.recyclerview.widget.RecyclerView
+import com.clearsky77.recyclerview.datas.ProfileData
+
+class ProfileAdapter(val profileList: ArrayList<ProfileData>) : RecyclerView.Adapter<ProfileAdapter.CustomViewHolder>()
