af75912ed16834684cb28bf0efcbceaeae96c582
clearsky77 <clearsky3264@gmail.com>
2021-10-23 01:26:02 +0900

    CustomViewHolder 작성

--- app/src/main/java/com/clearsky77/recyclerview/adapter/ProfileAdapter.kt
+++ app/src/main/java/com/clearsky77/recyclerview/adapter/ProfileAdapter.kt
@@ -2,26 +2,25 @@ package com.clearsky77.recyclerview.adapter
 
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
+import com.clearsky77.recyclerview.R
 import com.clearsky77.recyclerview.datas.ProfileData
 
 class ProfileAdapter(val profileList: ArrayList<ProfileData>) : RecyclerView.Adapter<ProfileAdapter.CustomViewHolder>() {
-    override fun onCreateViewHolder(
-        parent: ViewGroup,
-        viewType: Int
-    ): ProfileAdapter.CustomViewHolder {
-        TODO("Not yet implemented")
+    override fun onCreateViewHolder( parent: ViewGroup, viewType: Int ): ProfileAdapter.CustomViewHolder {
     }
 
     override fun onBindViewHolder(holder: ProfileAdapter.CustomViewHolder, position: Int) {
-        TODO("Not yet implemented")
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
     }
 
     class CustomViewHolder(itemView : View): RecyclerView.ViewHolder(itemView) {
-
+        val gender = itemView.findViewById<ImageView>(R.id.profileImg)
+        val name = itemView.findViewById<ImageView>(R.id.nameTxt)
+        val age = itemView.findViewById<ImageView>(R.id.ageTxt)
+        val job = itemView.findViewById<ImageView>(R.id.jobTxt)
     }
 }
