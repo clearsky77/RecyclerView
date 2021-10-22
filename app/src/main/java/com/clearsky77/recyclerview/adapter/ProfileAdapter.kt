1c3858f572318bcb76daf496a1151d078b6560d3
clearsky77 <clearsky3264@gmail.com>
2021-10-23 01:12:07 +0900

    필수 구현 1. 임플리먼트 멤버 2. CustomViewHolder

--- app/src/main/java/com/clearsky77/recyclerview/adapter/ProfileAdapter.kt
+++ app/src/main/java/com/clearsky77/recyclerview/adapter/ProfileAdapter.kt
@@ -1,6 +1,27 @@
 package com.clearsky77.recyclerview.adapter
 
+import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.clearsky77.recyclerview.datas.ProfileData
 
-class ProfileAdapter(val profileList: ArrayList<ProfileData>) : RecyclerView.Adapter<ProfileAdapter.CustomViewHolder>()
+class ProfileAdapter(val profileList: ArrayList<ProfileData>) : RecyclerView.Adapter<ProfileAdapter.CustomViewHolder>() {
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): ProfileAdapter.CustomViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ProfileAdapter.CustomViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    class CustomViewHolder(itemView : View): RecyclerView.ViewHolder(itemView) {
+
+    }
+}
