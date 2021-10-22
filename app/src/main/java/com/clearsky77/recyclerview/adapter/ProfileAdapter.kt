4a0d99f89a833b1fff44fca1a1410b558f5da6a2
clearsky77 <clearsky3264@gmail.com>
2021-10-23 01:45:13 +0900

    CustomViewHolder 작성 - holder로 틀 만들기

--- app/src/main/java/com/clearsky77/recyclerview/adapter/ProfileAdapter.kt
+++ app/src/main/java/com/clearsky77/recyclerview/adapter/ProfileAdapter.kt
@@ -1,26 +1,33 @@
 package com.clearsky77.recyclerview.adapter
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.clearsky77.recyclerview.R
 import com.clearsky77.recyclerview.datas.ProfileData
 
-class ProfileAdapter(val profileList: ArrayList<ProfileData>) : RecyclerView.Adapter<ProfileAdapter.CustomViewHolder>() {
+class ProfileAdapter(val profileList: ArrayList<ProfileData>) : RecyclerView.Adapter<ProfileAdapter.CustomViewHolder>()
+{
+
     override fun onCreateViewHolder( parent: ViewGroup, viewType: Int ): ProfileAdapter.CustomViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.list_item, parent, false)
+        return CustomViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return profileList.size // 크기가 얼마인지 알려주기
     }
 
     override fun onBindViewHolder(holder: ProfileAdapter.CustomViewHolder, position: Int) {
     }
 
-    override fun getItemCount(): Int {
-    }
-
     class CustomViewHolder(itemView : View): RecyclerView.ViewHolder(itemView) {
         val gender = itemView.findViewById<ImageView>(R.id.profileImg)
-        val name = itemView.findViewById<ImageView>(R.id.nameTxt)
-        val age = itemView.findViewById<ImageView>(R.id.ageTxt)
-        val job = itemView.findViewById<ImageView>(R.id.jobTxt)
+        val name = itemView.findViewById<TextView>(R.id.nameTxt)
+        val age = itemView.findViewById<TextView>(R.id.ageTxt)
+        val job = itemView.findViewById<TextView>(R.id.jobTxt)
     }
 }
